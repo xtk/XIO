caf365ce10f6872bffa74f5e2bd2d9b98a45fb49
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2013-06-17 12:13:22 -0400

    Webworker example working.

--- examples/webworker.js
+++ examples/webworker.js
@@ -1,2 +1,22 @@
+self.window = self;
 importScripts('../bin/xio.js');
 
+self.onmessage = function(e) {
+
+  var _file = e.data;
+
+  X.io.load(_file);
+
+  X.io.oncomplete = function() {
+    self.postMessage(X.io.get(_file));
+  };
+
+  X.io.onloading = function(id, progress) {
+
+    if (!id) {
+      self.postMessage(progress);
+    }
+
+  };
+
+};
